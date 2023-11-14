e7658be9c479597814c63303d8e27e3b3d0fa9c7
RobertGV00 <robertgvclase@gmail.com>
2023-11-14 16:39:45 +0100

    actualización

---
+++ clases/ejemplojavascript/script.js
@@ -0,0 +1,8 @@
+alert ("Hola mundo en JavaScript en otro fichero"); // esto es un comentario en JavaScript
+/* Comentario
+de
+varias 
+lineas
+*/
+alert ("Hola mundo en JavaScript en otro fichero en mayusculas");
+console.log ("Esto es un mensaje por consola y la primera que hacemos por JavaScript")
